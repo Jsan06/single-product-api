c8cf787c0baf56dae406a1c05915005dfaca0bf7
John Santhosh <santosh.johnn06.js@gmail.com>
2021-01-27 19:28:23 +0530

    add server

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const router1 = jsonServer.router1('db1.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
-server.use(router1);
 
 server.listen(port);
